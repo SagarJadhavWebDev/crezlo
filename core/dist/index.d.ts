3eddf053f32f135441933f24577a259553327a58
Mudassir Khan <khanmudassir124@gmail.com>
2025-07-16 11:02:23 +0530

    release: v1.0.15

--- core/dist/index.d.ts
+++ core/dist/index.d.ts
@@ -1,6 +1,1 @@
 import "./styles/index.css";
-export * from "./components";
-export * from "./context-providers";
-export * from "./hooks";
-export * from "./utils";
-export * from "./types";
--- core/src/index.ts
+++ core/src/index.ts
@@ -2,17 +2,17 @@
 import "./styles/index.css";
 
 // Export components
-export * from "./components";
+// export * from "./components";
 
-// Export contexts and providers
-export * from "./context-providers";
+// // Export contexts and providers
+// export * from "./context-providers";
 
-// Export hooks
-export * from "./hooks";
+// // Export hooks
+// export * from "./hooks";
 
-// Export utilities
-export * from "./utils";
+// // Export utilities
+// export * from "./utils";
 
-// Export types
-export * from "./types";
+// // Export types
+// export * from "./types";
 
