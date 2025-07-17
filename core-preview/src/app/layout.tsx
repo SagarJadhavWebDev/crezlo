e8c346585e43d5779703ea963aec3db0e3a62366
Mudassir Khan <khanmudassir124@gmail.com>
2025-07-17 10:15:30 +0530

    release: v1.0.18

--- core-preview/src/app/layout.tsx
+++ core-preview/src/app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { RootLayout } from "core/components";
-import { AppProvider, AuthProvider } from "core/context-providers";
+import { RootLayout } from "crezlo-core/components";
 import "@core/dist/index.css";
 export default function RootPreviewLayout({
   children,
