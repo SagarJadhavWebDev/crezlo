6c21bf18d350a6c6771dcb7ab714bf0c82de9ec9
Mudassir Khan <khanmudassir124@gmail.com>
2025-07-24 15:05:43 +0530

    release: v1.0.40

--- core/dist/components/common/pagination-numeric.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-interface PaginationNumericProps {
-    currentPage?: number;
-    totalPages?: number;
-    onPageChange?: (page: number) => void;
-}
-declare const PaginationNumeric: React.FC<PaginationNumericProps>;
-export default PaginationNumeric;
